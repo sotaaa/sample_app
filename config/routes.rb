053ae707a0a74201c88530c844ddeb03ba2117ed
Satoshi Sota <sotaaa@gmail.com>
2017-12-12 07:40:10 +0000

    add user micropost, feed function, destroy function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   devise_for :users, :controllers => { :registrations => 'registrations'}
   resources :users, only: [:show, :index, :destroy]
+  resources :microposts, only: [:create, :destroy]
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
