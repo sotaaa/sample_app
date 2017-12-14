ac8370d05cded5cf92481fe3cbda571b13a98598
Satoshi Sota <sotaaa@gmail.com>
2017-12-14 06:15:47 +0000

    add the follow and follower interface. the function of follow and follower is going to be added in next step

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 SampleApp::Application.routes.draw do
   devise_for :users, :controllers => { :registrations => 'registrations'}
-  resources :users, only: [:show, :index, :destroy]
+  resources :users, only: [:show, :index, :destroy] do
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :microposts, only: [:create, :destroy]
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
