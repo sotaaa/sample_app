10d250e98b848f30f5855ce5d2ff143499f2c939
Satoshi Sota <sotaaa@gmail.com>
2017-12-14 07:59:07 +0000

    add the follow/unfollow function

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,19 @@
 class RegistrationsController < Devise::RegistrationsController
 
+before_action :authenticate_user!
+
+  def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    redirect_to @user
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    redirect_to @user
+  end
+  
   def after_sign_up_path_for(resource)
     user_path(resource)
   end
